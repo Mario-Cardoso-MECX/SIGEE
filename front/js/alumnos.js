929ea7f5d3250951e82c655c7eddf8e3b4e05136
Mario Cardoso <xcme044@gmail.com>
2026-03-04 19:15:46 -0600

    Correcion de erro a la base de datos

--- front/js/alumnos.js
+++ front/js/alumnos.js
@@ -33,7 +33,7 @@ async function cargarAlumnos() {
     const puedeEditar = rol === 'Admin' || rol === 'Secretaria';
 
     try {
-        const response = await fetch('${API_URL}/Usuarios/alumnos');
+        const response = await fetch(`${API_URL}/Usuarios/alumnos`);
         if (!response.ok) throw new Error("Error al obtener alumnos");
 
         const alumnos = await response.json();
@@ -80,7 +80,7 @@ async function registrarAlumno() {
     const resultado = document.getElementById('resultadoRegistro');
 
     try {
-        const response = await fetch('${API_URL}/Usuarios/crear', {
+        const response = await fetch(`${API_URL}/Usuarios/crear`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ 
